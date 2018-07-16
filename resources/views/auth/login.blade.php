de6c2d499616c9c445a49e6dafd9786c943e8c92
ayeshNipun <nipun.yesh@gmail.com>
2018-07-16 22:58:14 +0530

    login effects

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,197 @@
+
+    <form action="{{ route('login') }}" method="post">
+            @csrf
+
+        <div class="body"></div>
+        <div class="grad"></div>
+        <div class="header">
+            <div>Tec<span>Hour</span></div>
+        </div>
+        <br>
+        <div class="login">
+            <div>
+                <input id="email" placeholder="E Mail" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}"><br>
+                @if ($errors->has('email'))
+                <span class="invalid-feedback">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+                @endif
+            </div>
+
+            <div>
+                <input id="password" type="password" placeholder="Password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required><br>
+                @if ($errors->has('password'))
+                    <span class="invalid-feedback">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+            </div>
+            <br>
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
+                </label>
+            </div>
+            <br>
+            <div>
+                <input type="submit" value="Login">
+                <br><br>
+                <a class="btn-link" href="{{ route('password.request') }}">
+                    {{ __('Forgot Your Password?') }}
+                </a>
+            </div>
+
+                
+        </div>
+    </form>
+
+
+<style>
+    .btn-link{
+        text-decoration: none;
+    }
+    @import url(https://fonts.googleapis.com/css?family=Exo:100,200,400);
+    @import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:700,400,300);
+
+    body{
+        overflow: hidden;
+        background-image: url('/img/background.png');
+        color: #636b6f;
+        font-family: 'Raleway', sans-serif;
+        font-weight: 100;
+        height: 100vh;
+        margin: 0;
+    }
+    
+    .grad{
+    	position: absolute;
+    	top: -20px;
+    	left: -20px;
+    	right: -40px;
+    	bottom: -40px;
+    	width: auto;
+    	height: auto;
+    	background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0)), color-stop(100%,rgba(0,0,0,0.65))); /* Chrome,Safari4+ */
+    	z-index: 1;
+    	opacity: 0.7;
+    }
+    
+    .header{
+    	position: absolute;
+    	top: calc(50% - 35px);
+    	left: calc(50% - 255px);
+    	z-index: 2;
+    }
+    
+    .header div{
+    	float: left;
+    	color: #fff;
+    	font-family: 'Exo', sans-serif;
+    	font-size: 35px;
+    	font-weight: 200;
+    }
+    
+    .header div span{
+    	color: #5379fa !important;
+    }
+    
+    .login{
+    	position: absolute;
+    	top: calc(50% - 75px);
+    	left: calc(50% - 50px);
+    	height: 150px;
+    	width: 350px;
+    	padding: 10px;
+    	z-index: 2;
+    }
+    
+    .login input[type=email]{
+    	width: 250px;
+    	height: 30px;
+    	background: transparent;
+    	border: 1px solid rgba(255,255,255,0.6);
+    	border-radius: 2px;
+    	color: #fff;
+    	font-family: 'Exo', sans-serif;
+    	font-size: 16px;
+    	font-weight: 400;
+    	padding: 4px;
+    }
+    
+    .login input[type=password]{
+    	width: 250px;
+    	height: 30px;
+    	background: transparent;
+    	border: 1px solid rgba(255,255,255,0.6);
+    	border-radius: 2px;
+    	color: #fff;
+    	font-family: 'Exo', sans-serif;
+    	font-size: 16px;
+    	font-weight: 400;
+    	padding: 4px;
+    	margin-top: 10px;
+    }
+    
+    .login input[type=submit]{
+    	width: 250px;
+    	height: 35px;
+    	background: #fff;
+    	border: 1px solid #fff;
+    	cursor: pointer;
+    	border-radius: 2px;
+    	color: #a18d6c;
+    	font-family: 'Exo', sans-serif;
+    	font-size: 16px;
+    	font-weight: 400;
+    	padding: 6px;
+    	margin-top: 10px;
+    }
+    
+    .login input[type=submit]:hover{
+    	opacity: 0.8;
+    }
+    
+    .login input[type=submit]:active{
+    	opacity: 0.6;
+    }
+    
+    .login input[type=email]:focus{
+    	outline: none;
+    	border: 1px solid rgba(255,255,255,0.9);
+    }
+    
+    .login input[type=password]:focus{
+    	outline: none;
+    	border: 1px solid rgba(255,255,255,0.9);
+    }
+    
+    .login input[type=submit]:focus{
+    	outline: none;
+    }
+    
+    ::-webkit-input-placeholder{
+       color: rgba(255,255,255,0.6);
+    }
+    
+    ::-moz-input-placeholder{
+       color: rgba(255,255,255,0.6);
+    }
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 {{-- @extends('layouts.app')
 
 @section('content') --}}
@@ -70,7 +264,7 @@
     </div>
 </div> --}}
 {{-- @endsection --}}
-
+{{-- 
 <section id="hire">
     <h1>LOGIN</h1>
     
@@ -319,47 +513,48 @@
     }
 </style>
 
+
 <script>
-     // Input Lock
-    $('textarea').blur(function () {
-        $('#hire textarea').each(function () {
-            $this = $(this);
-            if ( this.value !== '' ) {
-              $this.addClass('focused');
-              $('textarea + label + span').css({'opacity': 1});
-            }
-            else {
-              $this.removeClass('focused');
-              $('textarea + label + span').css({'opacity': 0});
-            }
-        });
+// Input Lock
+$('textarea').blur(function () {
+    $('#hire textarea').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('textarea + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('textarea + label + span').css({'opacity': 0});
+        }
     });
+});
 
-    $('#hire .field:first-child input').blur(function () {
-        $('#hire .field:first-child input').each(function () {
-            $this = $(this);
-            if ( this.value !== '' ) {
-              $this.addClass('focused');
-              $('.field:first-child input + label + span').css({'opacity': 1});
-            }
-            else {
-              $this.removeClass('focused');
-              $('.field:first-child input + label + span').css({'opacity': 0});
-            }
-        });
+$('#hire .field:first-child input').blur(function () {
+    $('#hire .field:first-child input').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('.field:first-child input + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('.field:first-child input + label + span').css({'opacity': 0});
+        }
     });
+});
 
-    $('#hire .field:nth-child(2) input').blur(function () {
-        $('#hire .field:nth-child(2) input').each(function () {
-            $this = $(this);
-            if ( this.value !== '' ) {
-              $this.addClass('focused');
-              $('.field:nth-child(2) input + label + span').css({'opacity': 1});
-            }
-            else {
-              $this.removeClass('focused');
-              $('.field:nth-child(2) input + label + span').css({'opacity': 0});
-            }
-        });
+$('#hire .field:nth-child(2) input').blur(function () {
+    $('#hire .field:nth-child(2) input').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('.field:nth-child(2) input + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('.field:nth-child(2) input + label + span').css({'opacity': 0});
+        }
     });
-</script>
+});
+</script> --}}
