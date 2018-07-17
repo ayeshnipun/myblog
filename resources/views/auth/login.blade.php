dd835a433dde808519725097c8f28ee49c14724a
ayeshNipun <nipun.yesh@gmail.com>
2018-07-17 11:06:09 +0530

    fixes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,179 +1,97 @@
 
-    <form action="{{ route('login') }}" method="post">
-            @csrf
+@extends('layouts.app') 
 
-        <div class="body"></div>
-        <div class="grad"></div>
-        <div class="header">
-            <div>Tec<span>Hour</span></div>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header text-center">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-        <br>
-        <div class="login">
-            <div>
-                <input id="email" placeholder="E Mail" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}"><br>
-                @if ($errors->has('email'))
-                <span class="invalid-feedback">
-                    <strong>{{ $errors->first('email') }}</strong>
-                </span>
-                @endif
-            </div>
-
-            <div>
-                <input id="password" type="password" placeholder="Password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required><br>
-                @if ($errors->has('password'))
-                    <span class="invalid-feedback">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                @endif
-            </div>
-            <br>
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
-                </label>
-            </div>
-            <br>
-            <div>
-                <input type="submit" value="Login">
-                <br><br>
-                <a class="btn-link" href="{{ route('password.request') }}">
-                    {{ __('Forgot Your Password?') }}
-                </a>
-            </div>
-
-                
-        </div>
-    </form>
+    </div>
+</div>
+@endsection
 
 
 <style>
-    .btn-link{
-        text-decoration: none;
-    }
-    @import url(https://fonts.googleapis.com/css?family=Exo:100,200,400);
-    @import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:700,400,300);
-
-    body{
-        overflow: hidden;
-        background-image: url('/img/background.png');
-        color: #636b6f;
-        font-family: 'Raleway', sans-serif;
-        font-weight: 100;
-        height: 100vh;
-        margin: 0;
+    .card{
+        background-color: transparent !important;
     }
 
-    .grad{
-    	position: absolute;
-    	top: -20px;
-    	left: -20px;
-    	right: -40px;
-    	bottom: -40px;
-    	width: auto;
-    	height: auto;
-    	background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0)), color-stop(100%,rgba(0,0,0,0.65))); /* Chrome,Safari4+ */
-    	z-index: 1;
-    	opacity: 0.7;
+    .card-header{
+        color: white !important;
+        font-size: 1cm;
+        text-transform: uppercase;
     }
 
-    .header{
-    	position: absolute;
-    	top: calc(50% - 35px);
-    	left: calc(50% - 255px);
-    	z-index: 2;
+    label{
+        color: white;
     }
 
-    .header div{
-    	float: left;
-    	color: #fff;
-    	font-family: 'Exo', sans-serif;
-    	font-size: 35px;
-    	font-weight: 200;
+    input[type="email"]{
+        background-color: transparent !important;
+        color: white !important;
     }
 
-    .header div span{
-    	color: #5379fa !important;
-    }
-
-    .login{
-    	position: absolute;
-    	top: calc(50% - 75px);
-    	left: calc(50% - 50px);
-    	height: 150px;
-    	width: 350px;
-    	padding: 10px;
-    	z-index: 2;
-    }
-
-    .login input[type=email]{
-    	width: 250px;
-    	height: 30px;
-    	background: transparent;
-    	border: 1px solid rgba(255,255,255,0.6);
-    	border-radius: 2px;
-    	color: #fff;
-    	font-family: 'Exo', sans-serif;
-    	font-size: 16px;
-    	font-weight: 400;
-    	padding: 4px;
-    }
-
-    .login input[type=password]{
-    	width: 250px;
-    	height: 30px;
-    	background: transparent;
-    	border: 1px solid rgba(255,255,255,0.6);
-    	border-radius: 2px;
-    	color: #fff;
-    	font-family: 'Exo', sans-serif;
-    	font-size: 16px;
-    	font-weight: 400;
-    	padding: 4px;
-    	margin-top: 10px;
-    }
-
-    .login input[type=submit]{
-    	width: 250px;
-    	height: 35px;
-    	background: #fff;
-    	border: 1px solid #fff;
-    	cursor: pointer;
-    	border-radius: 2px;
-    	color: #a18d6c;
-    	font-family: 'Exo', sans-serif;
-    	font-size: 16px;
-    	font-weight: 400;
-    	padding: 6px;
-    	margin-top: 10px;
-    }
-
-    .login input[type=submit]:hover{
-    	opacity: 0.8;
-    }
-
-    .login input[type=submit]:active{
-    	opacity: 0.6;
-    }
-
-    .login input[type=email]:focus{
-    	outline: none;
-    	border: 1px solid rgba(255,255,255,0.9);
-    }
-
-    .login input[type=password]:focus{
-    	outline: none;
-    	border: 1px solid rgba(255,255,255,0.9);
-    }
-
-    .login input[type=submit]:focus{
-    	outline: none;
-    }
-
-    ::-webkit-input-placeholder{
-       color: rgba(255,255,255,0.6);
-    }
-
-    ::-moz-input-placeholder{
-       color: rgba(255,255,255,0.6);
+    input[type="password"]{
+        background-color: transparent !important;
+        color: white !important;
     }
 </style>
